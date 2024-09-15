975afb40079b02b7a7d5637e6e4855a36047ec95
MOHIT-SOLANKII <mohitslnki773@gmail.com>
2024-09-15 20:52:27 +0530

    Final routing setup for Login and Register

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
 
 
 
 function App() {
 
-  const [allRoutes, setAllRoutes] = useState([])
-  // As of noe allRoutes has no routes, so it is an empty array.
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes])
+  // As of now allRoutes has all routes contained in publicRoutes.
+
+
+  // console.log(allRoutes);
+  
 
   return <Router allRoutes={allRoutes} />
   // We can update allRoutes with the routes that we want to render in our application. We can pass allRoutes as a prop to the Router component.
